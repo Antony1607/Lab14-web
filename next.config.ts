6c95623baf583c88dbe3bff8da69d7dc9a0f8ed8
TLarem <acuario52ha@gmail.com>
2026-06-24 22:17:25 -0500

    actualizacion

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+      // ⚡ EL TRUCO: Cuando tú pongas "blog-sitemap" en Search Console, 
+      // Next llamará silenciosamente a tu API y le dará XML puro a Google.
+      {
+        source: '/blog-sitemap', 
+        destination: '/api/sitemap',
+      }
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
